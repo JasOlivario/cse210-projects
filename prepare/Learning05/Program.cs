73b8b00dcf8a04eedeb701417cb7232b57d3358e
JasOlivario <joshuesamolivario@gmail.com>
2023-11-12 03:51:01 +0800

    Additional modifications

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+
+        Square s = new Square("blue", 13);
+        shapes.Add(s);
+
+        Rectangle r = new Rectangle("orange", 14, 3);
+        shapes.Add(r);
+
+        Circle c = new Circle("purple", 15);
+        shapes.Add(c);
+
+
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
+
     }
+
 }
